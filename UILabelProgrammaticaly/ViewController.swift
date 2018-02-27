1c34b1596027f56f10fb8fcf16fafb8552444d9c
Peter Zaporowski <peterzaporowski@Peters-Mac.local>
2018-02-27 13:55:17 +0100

    Initial Commit

---
+++ UILabelProgrammaticaly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UILabelProgrammaticaly
+//
+//  Created by Peter Zaporowski on 27.02.2018.
+//  Copyright © 2018 Peter Zaporowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
